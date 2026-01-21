f7809a16a94291c1f91e8b5aee767793f1574c6b
Ventsislav Ivanov <ventsislav.ivanov89@gmail.com>
2026-01-21 17:42:46 +0200

    Make footer year dynamic
    Replaced the hardcoded year in the footer with a dynamic value based on the current year to ensure it stays up to date automatically.

--- front-end-react/src/components/footer/Footer.jsx
+++ front-end-react/src/components/footer/Footer.jsx
@@ -1,7 +1,10 @@
 export default function Footer() {
+	const date = new Date();
+	const  year = date.getFullYear();
+	
 	return (
 		<footer className="text-center py-3 bg-primary text-white">
-			&copy; 2025 MovieSearch. All rights reserved.
+			&copy; {year} MovieSearch. All rights reserved.
 		</footer>
 	);
 }
