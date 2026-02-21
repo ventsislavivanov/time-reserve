6739d7ece395407b8c7d4452b0c6bda73bb113ad
Ventsislav Ivanov <ventsislav.ivanov89@gmail.com>
2026-02-21 14:33:23 +0200

    add server configuration to l5-swagger for environment-specific settings

--- back-end/app/Swagger/OpenApi.php
+++ back-end/app/Swagger/OpenApi.php
@@ -8,10 +8,6 @@ use OpenApi\Attributes as OA;
 	version: "1.0.0",
 	title: "My API Documentation"
 )]
-#[OA\Server(
-	url: "http://10.66.4.47:8989",
-	description: "Local development server"
-)]
 #[OA\SecurityScheme(
 	securityScheme: "sanctum",
 	type: "http",
