7989ca95176bcba26150bcb1ac205876aaa45575
Ventsislav Ivanov <ventsislav.ivanov89@gmail.com>
2026-04-13 01:29:08 +0300

    Hide header on staff login route
    Conditionally render the app header for the staff login page. Updated app.html to render <app-header> only when hideHeader is false. In app.ts added Router injection, NavigationEnd and rxjs filter usage, and a hideHeader signal that is toggled on NavigationEnd when the URL starts with '/staff/login'. Also added necessary imports (inject, Router, NavigationEnd, filter) and wiring in the App constructor.

--- front-end-angular/src/app/app.ts
+++ front-end-angular/src/app/app.ts
@@ -1,9 +1,10 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { Component, inject, signal } from '@angular/core';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { filter } from 'rxjs';
 
 import { Header, Footer } from './shared/components';
+
 
 
 @Component({
@@ -18,5 +19,16 @@ import { Header, Footer } from './shared/components';
   styleUrl: './app.css'
 })
 export class App {
+  private router = inject(Router);
+
   protected readonly title = signal('front-end-angular');
+  hideHeader = signal(false);
+
+  constructor() {
+    this.router.events
+      .pipe(filter(e => e instanceof NavigationEnd))
+      .subscribe((e: NavigationEnd) => {
+        this.hideHeader.set(e.urlAfterRedirects.startsWith('/staff/login'));
+      });
+  }
 }
