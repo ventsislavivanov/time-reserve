a1c07ec6ba3ff0cf26a649e69ff71953a8dd1afd
Ventsislav Ivanov <ventsislav.ivanov89@gmail.com>
2026-04-13 19:40:38 +0300

    Add reusable loading component
    Introduce a reusable `app-loading` component with a spinner and visually hidden accessibility text. Includes its HTML template and a minimal TypeScript class for integration.

---
+++ front-end-angular/src/app/shared/components/ui/display/loading/loading.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loading',
+  imports: [],
+  templateUrl: './loading.html',
+})
+export class Loading {}
--- front-end-angular/src/app/shared/components/ui/index.ts
+++ front-end-angular/src/app/shared/components/ui/index.ts
@@ -1,6 +1,7 @@
 export * from './navigation/nav-link/nav-link';
 
 export * from './display/appointment-status/appointment-status'
+export * from './display/loading/loading'
 
 export * from './form/form-field/form-field';
 export * from './form/radio-group/radio-group';
