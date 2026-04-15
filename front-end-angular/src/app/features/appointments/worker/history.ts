fa5ac74b52ec102545c059036561ba76e6f74cbf
Ventsislav Ivanov <ventsislav.ivanov89@gmail.com>
2026-04-15 20:58:05 +0300

    Add worker history page

--- front-end-angular/src/app/features/appointments/worker/history.ts
+++ front-end-angular/src/app/features/appointments/worker/history.ts
@@ -1,8 +1,44 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit, signal} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ToastrService } from 'ngx-toastr';
+import { faEye } from '@fortawesome/free-solid-svg-icons';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+
+import { AppointmentStatus, Loading } from "../../../shared/components/ui";
+import { AppointmentsService } from '../appointments.service';
+import { Appointment, AppointmentsResponse } from '../appointment.model';
 
 @Component({
   selector: 'history',
-  imports: [],
+  imports: [
+    CommonModule,
+    Loading,
+    AppointmentStatus,
+    FaIconComponent
+  ],
   templateUrl: './history.html',
 })
-export class History {}
+export class History implements OnInit{
+  private appointmentsService = inject(AppointmentsService);
+  private toastr = inject(ToastrService);
+
+  faEye = faEye;
+
+  appointments  = signal<Appointment[]>([]);
+  isLoading = signal<boolean>(true);
+
+   ngOnInit() {
+    this.isLoading.set(true);
+    this.appointmentsService.getStaffAppointments().subscribe({
+      next: (res: AppointmentsResponse) => {
+        this.appointments.set(res.data);
+      },
+      error: () => this.toastr.error('Failed to load history'),
+      complete: () => this.isLoading.set(false)
+    })
+  }
+
+  onViewDetails(id: number) {
+    console.log(id)
+  }
+}
