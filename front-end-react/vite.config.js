0df6709af68387707d21a6fd85579adadb1199c0
Ventsislav Ivanov <ventsislav.ivanov89@gmail.com>
2026-07-17 14:17:13 +0300

    add allowedHosts in react

--- front-end-react/vite.config.js
+++ front-end-react/vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  const allowedHostsRaw = env.VITE_ALLOWED_HOSTS || 'localhost'
+  const allowedHosts = allowedHostsRaw.split(',').map(host => host.trim())
+
+  return {
+    plugins: [react()],
+    server: {
+      allowedHosts: allowedHosts,
+      host: '0.0.0.0',
+      port: 80
+    }
+  }
 })
